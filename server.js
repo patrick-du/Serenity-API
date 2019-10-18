a89145bd05a36cd7f665c148a92f1d21b3acc74b
Patrick Du <patrickduu@gmail.com>
2019-10-18 15:51:54 -0400

    cors???

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ app.use(
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+        "Access-Control-Allow-Origin", '*'
+        //"Access-Control-Allow-Headers",
+        //"Origin, X-Requested-With, Content-Type, Accept"
     );
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     next();
