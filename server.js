49b6f074d1a02d6861cbcf168c305c47c7ddea70
Patrick Du <patrickduu@gmail.com>
2019-10-18 09:31:15 -0400

    test

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(
         extended: false
     })
 );
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(bodyParser.json());
 
 // app.get('*', (req,res) => {
