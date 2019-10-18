3d99c25faaab98f4da56a6251e66aee12ee1fbf8
Patrick Du <patrickduu@gmail.com>
2019-10-18 16:08:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,10 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     next();
-});
+  });
 
 app.use(bodyParser.json());
 
